0cd749c6db2226b281f6771b85c609dd099cfd2e
coderlawyerr <99267937+coderlawyerr@users.noreply.github.com>
2023-11-01 20:12:10 +0300

    commit 6

--- lib/main.dart
+++ lib/main.dart
@@ -43,8 +43,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
