b4d0ef67bd30eb8e46b30f7ed9a6b67821a0dcc3
coderlawyerr <99267937+coderlawyerr@users.noreply.github.com>
2023-11-01 20:09:01 +0300

    çapraz düzenleme

--- lib/main.dart
+++ lib/main.dart
@@ -42,9 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
